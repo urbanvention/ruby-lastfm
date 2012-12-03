7f4f7d7ece7d9a8a70d6bb7e9c6fed30bbcee688
Moritz Lawitschka <moritz@urbanvention.com>
2012-12-03 21:21:24 +0100

    changed Artist methods to work with MusicBrainz ID

--- lib/lastfm/method_category/artist.rb
+++ lib/lastfm/method_category/artist.rb
@@ -1,35 +1,35 @@
 class Lastfm
   module MethodCategory
     class Artist < Base
-      regular_method :get_top_tracks, [:artist], [] do |response|
+      regular_method :get_top_tracks, [:mbid], [] do |response|
         response.xml['toptracks']['track']
       end
 
-      regular_method :get_top_albums, [:artist], [] do |response|
+      regular_method :get_top_albums, [:mbid], [] do |response|
         response.xml['topalbums']['album']
       end
 
-      regular_method :get_info, [:artist], [] do |response|
+      regular_method :get_info, [:mbid], [] do |response|
         response.xml['artist']
       end
 
-      regular_method :get_events, [:artist], [] do |response|
+      regular_method :get_events, [:mbid], [] do |response|
         response.xml['events']['event']
       end
 
-      regular_method :get_images, [:artist], [] do |response|
+      regular_method :get_images, [:mbid], [] do |response|
         response.xml['images']['image']
       end
 
-      regular_method :get_similar, [:artist], [] do |response|
+      regular_method :get_similar, [:mbid], [] do |response|
         response.xml['similarartists']['artist']
       end
 
-      regular_method :get_tags, [:artist], [[:user, nil], [:mbid, nil], [:autocorrect, nil]] do |response|
+      regular_method :get_tags, [:mbid], [[:user, nil], [:mbid, nil], [:autocorrect, nil]] do |response|
         response.xml['tags']['tag']
       end
-      
-      regular_method :get_top_fans, [:artist], [] do |response|
+
+      regular_method :get_top_fans, [:mbid], [] do |response|
         response.xml['topfans']['user']
       end
 
